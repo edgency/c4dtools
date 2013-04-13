7e0f99a722ad9dec8f8737e5dd254d7407842773
Niklas Rosenstein <rosensteinniklas@googlemail.com>
2013-04-13 19:52:13 +0200

    Re-impl of c4dtools.misc.boundingbox, !!! Interface changed

--- c4dtools/misc/boundingbox.py
+++ c4dtools/misc/boundingbox.py
@@ -40,117 +40,76 @@ or by :class:`c4d.BaseObject` instances.
 """
 
 import c4d
-import copy
-from c4dtools.utils import clsname, vmin, vmax
+from c4dtools.utils import vmin, vmax, ensure_type
 
 class AABB(object):
     r"""
-    This class makes it easy to compute the bounding-box of a set of
-    points or even objects. AABB is short for "axis-aligned bounding-box".
+    Expandable bounding-box system.
 
-    Example for Script Manager:
+    *Changed in 1.2.9*: Re-implementation, interface has changed.
 
-    .. code-block:: python
-
-        from c4dtools.misc.boundingbox import AABB
-        box = AABB()
-        box.expand(op, recursive=True)
-        print box.midpoint
-        print box.size
-
-    The bounding box is *always* calculated from global coordinates and
-    is translated with the matrix in the translation_matrix slot. The
-    translation can not be performed after expansion of the bounding-
-    box, the translation_matrix must therefore be set *before*
-    expand() or expand_point() is called.
-
-    .. code-block:: python
-
-        from c4dtools.misc.boundingbox import AABB
-        box = AABB(translation_matrix=~op.GetMg())
-        box.expand(mg, recursive=True)
-        print box.midpoint
-        print box.size
+    - ``expand()`` is now called ``expand_object()``
+    - ``expand_point()`` is now called ``expand()``
+    - ``translation_matrix`` is not called ``translation``
+    - Initialization is done with the first call to :meth:`expand`
+    - Polygon objects are now treated in a special way
     """
 
-    def __init__(self, min_v=c4d.Vector(0), max_v=c4d.Vector(0),
-                 translation_matrix=c4d.Matrix()):
+    def __init__(self, translation=None):
         super(AABB, self).__init__()
-        self.min_v = copy.copy(min_v)
-        self.max_v = copy.copy(max_v)
-        self.translation_matrix = copy.copy(translation_matrix)
+        if translation is None:
+            translation = c4d.Matrix()
 
-    def expand(self, obj, recursive=False, init=False):
-        r"""
-        Expand the bounding-box by the passed :class:`c4d.BaseObject`
-        instance. The method can optionally continue recursively.
+        self.minv = None
+        self.maxv = None
+        self.init = False
+        self.translation = translation
 
-        *New in 1.2.9*: Added parameter *init*. Pass *True* for initializing
-        the bounding-box with this point.
+    def expand(self, point):
+        point = point * self.translation
+        if not self.init:
+            self.minv = c4d.Vector(point)
+            self.maxv = c4d.Vector(point)
+            self.init = True
+        else:
+            self.minv = vmin(self.minv, point)
+            self.maxv = vmax(self.maxv, point)
 
-        Raises: TypeError when *obj* is not an instance of
-                :class:`c4d.BaseObject`.
-        """
-
-        if not isinstance(obj, c4d.BaseObject):
-            raise TypeError('expected c4d.BaseObject, got %s.' % clsname(obj))
-
+    def expand_object(self, obj, recursive=False):
+        ensure_type(obj, c4d.BaseObject)
         mg = obj.GetMg()
-        mp = obj.GetMp()
-        bb = obj.GetRad()
-
-        V = c4d.Vector
-
-        self.expand_point(V(mp.x + bb.x, mp.y + bb.y, mp.z + bb.z) * mg, init)
-        self.expand_point(V(mp.x - bb.x, mp.y + bb.y, mp.z + bb.z) * mg)
-        self.expand_point(V(mp.x + bb.x, mp.y + bb.y, mp.z - bb.z) * mg)
-        self.expand_point(V(mp.x - bb.x, mp.y + bb.y, mp.z - bb.z) * mg)
-
-        self.expand_point(V(mp.x + bb.x, mp.y - bb.y, mp.z + bb.z) * mg)
-        self.expand_point(V(mp.x - bb.x, mp.y - bb.y, mp.z + bb.z) * mg)
-        self.expand_point(V(mp.x + bb.x, mp.y - bb.y, mp.z - bb.z) * mg)
-        self.expand_point(V(mp.x - bb.x, mp.y - bb.y, mp.z - bb.z) * mg)
+        
+        if isinstance(obj, c4d.PointObject):
+            for p in obj.GetAllPoints():
+                self.expand(p * mg)
+        else:
+            mp = obj.GetMp()
+            bb = obj.GetRad()
+    
+            V = c4d.Vector
+    
+            self.expand(V(mp.x + bb.x, mp.y + bb.y, mp.z + bb.z) * mg)
+            self.expand(V(mp.x - bb.x, mp.y + bb.y, mp.z + bb.z) * mg)
+            self.expand(V(mp.x + bb.x, mp.y + bb.y, mp.z - bb.z) * mg)
+            self.expand(V(mp.x - bb.x, mp.y + bb.y, mp.z - bb.z) * mg)
+    
+            self.expand(V(mp.x + bb.x, mp.y - bb.y, mp.z + bb.z) * mg)
+            self.expand(V(mp.x - bb.x, mp.y - bb.y, mp.z + bb.z) * mg)
+            self.expand(V(mp.x + bb.x, mp.y - bb.y, mp.z - bb.z) * mg)
+            self.expand(V(mp.x - bb.x, mp.y - bb.y, mp.z - bb.z) * mg)
 
         if recursive:
             for child in obj.GetChildren():
-                self.expand(child, True, False)
-
-    def expand_point(self, point, init=False):
-        r"""
-        Expand the bounding-box by the passed c4d.Vector representing
-        a point in the 3-dimensional space.
-
-        *New in 1.2.9*: Added parameter *init*. Pass *True* for initializing
-        the bounding-box with this point.
-
-        Raises: TypeError when *point* is not an instance of
-                c4d.Vector.
-        """
-
-        if init:
-            self.min_v = c4d.Vector(point)
-            self.max_v = c4d.Vector(point)
-        else:
-            vmin(self.min_v, point * self.translation_matrix)
-            vmax(self.max_v, point * self.translation_matrix)
+                self.expand_object(child, True)
 
     @property
     def midpoint(self):
-        r"""
-        Calcuates and returns the midpoint of the bounding-box yet
-        created.
-        """
-
-        return (self.min_v + self.max_v) * 0.5
+        return (self.minv + self.maxv) * 0.5
 
     @property
     def size(self):
-        r"""
-        Returns the size of the bounding-box yet created. The size
-        plus the midpoint of the bounding-box is the upper-right-front
-        corner of the box.
-        """
+        return (self.maxv - self.minv) * 0.5
 
-        return (self.max_v - self.min_v) * 0.5
+
 
 
